2a352c83e860b3d4eca6fb797ae3821c987fb06b
aero <aerooneQ@yandex.ru>
2023-06-25 21:56:13 +0300

    Update test data

--- test_data/TestConsoleApp/TestConsoleApp/Program.cs
+++ test_data/TestConsoleApp/TestConsoleApp/Program.cs
@@ -1,3 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using ClassLibrary1;
+using ClassLibrary2;
+
+Console.WriteLine(ReferenceEquals(Class1Container.Resolve(), Class1Container.Resolve()));
+Console.WriteLine(ReferenceEquals(Class2Container.Resolve(), Class2Container.Resolve()));
+Console.WriteLine(ReferenceEquals(Class3Container.Resolve(), Class3Container.Resolve()));
+Console.WriteLine(ReferenceEquals(Class4Container.Resolve(), Class4Container.Resolve()));
+Console.WriteLine(ReferenceEquals(Class5Container.Resolve(), Class5Container.Resolve()));
+
 Console.WriteLine("Hello, World!");
