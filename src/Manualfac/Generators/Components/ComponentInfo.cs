0d2fb57107a3328adbf12dd5084be9539ca400d1
aero <aerooneQ@yandex.ru>
2023-06-28 01:21:53 +0300

    Removed not needed base class

--- src/Manualfac/Generators/Components/ComponentInfo.cs
+++ src/Manualfac/Generators/Components/ComponentInfo.cs
@@ -2,13 +2,19 @@ using Microsoft.CodeAnalysis;
 
 namespace Manualfac.Generators.Components;
 
-internal class ComponentInfo : ComponentInfoBase, IComponentInfo
+internal class ComponentInfo : IComponentInfo
 {
-  public override INamedTypeSymbol ComponentSymbol { get; }
-  public override HashSet<IComponentDependency> Dependencies { get; }
-  public override IReadOnlyList<(IComponentDependency Component, AccessModifier Modifier)> OrderedDependencies { get; }
+  public INamedTypeSymbol ComponentSymbol { get; }
+  public HashSet<IComponentDependency> Dependencies { get; }
+  public IReadOnlyList<(IComponentDependency Component, AccessModifier Modifier)> OrderedDependencies { get; }
   
+  public string TypeShortName => ComponentSymbol.Name;
+  public string FullName => Namespace is { } @namespace ? @namespace + "." + TypeShortName : TypeShortName;
   
+  // ReSharper disable once ReturnTypeCanBeNotNullable
+  public string? Namespace => ComponentSymbol.ContainingNamespace.Name;
+
+
   public ComponentInfo(
     INamedTypeSymbol componentSymbol, 
     IReadOnlyCollection<(IComponentDependency Component, AccessModifier Modifier)> dependencies)
@@ -16,5 +22,17 @@ internal class ComponentInfo : ComponentInfoBase, IComponentInfo
     ComponentSymbol = componentSymbol;
     Dependencies = new HashSet<IComponentDependency>(dependencies.Select(dep => dep.Component));
     OrderedDependencies = dependencies.OrderBy(dep => dep.Component.DependencyTypeSymbol.Name).ToList();
+  }
+  
+  
+  public IReadOnlyList<IComponentInfo> ResolveConcreteDependencies()
+  {
+    var concreteDependencies = new List<IComponentInfo>();
+    foreach (var (component, _) in OrderedDependencies)
+    {
+      concreteDependencies.AddRange(component.ResolveUnderlyingConcreteComponents());
+    }
+
+    return concreteDependencies;
   }
 }
