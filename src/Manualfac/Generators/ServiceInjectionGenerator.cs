9b0dfdad4a84b92cf0f8de65b699eb2cdac606fc
aero <aerooneQ@yandex.ru>
2023-06-30 01:40:54 +0300

    Remove stupid classes

--- src/Manualfac/Generators/ServiceInjectionGenerator.cs
+++ src/Manualfac/Generators/ServiceInjectionGenerator.cs
@@ -1,4 +1,6 @@
+using System.Text;
 using Manualfac.Generators.Components;
+using Manualfac.Generators.Models;
 using Microsoft.CodeAnalysis;
 
 namespace Manualfac.Generators;
@@ -26,20 +28,37 @@ public class ServiceInjectionGenerator : ISourceGenerator
     foreach (var componentInfo in components)
     {
       var assembly = componentInfo.ComponentSymbol.ContainingAssembly;
-      if (SymbolEqualityComparer.Default.Equals(assembly, context.Compilation.Assembly))
-      {
-        PartialComponentDefinitionGenerator.GenerateDependenciesPart(componentInfo, context);
-      }
+      if (!SymbolEqualityComparer.Default.Equals(assembly, context.Compilation.Assembly)) continue;
+      
+      var sb = new StringBuilder();
+      componentInfo.ToGeneratedFileModel().GenerateInto(sb, 0);
+    
+      context.AddSource($"{componentInfo.TypeShortName}.g", sb.ToString());
     }
   }
 
   private static void GenerateContainerBuilder(ComponentsStorage storage, GeneratorExecutionContext context)
   {
-    ContainerBuilderGenerator.GenerateContainerInitialization(storage, context);
+    var compilationAssembly = context.Compilation.Assembly;
+    foreach (var componentInfo in ComponentsTopologicalSorter.Sort(storage.AllComponents))
+    {
+      var componentAssembly = componentInfo.ComponentSymbol.ContainingAssembly;
+      if (!SymbolEqualityComparer.Default.Equals(compilationAssembly, componentAssembly)) continue;
+      
+      var sb = new StringBuilder();
+      new GeneratedComponentContainerModel(componentInfo).GenerateInto(sb, 0);
+    
+      context.AddSource($"{componentInfo.CreateContainerName()}.g", sb.ToString());
+    }
   }
 
   private static void GenerateContainerInitialization(ComponentsStorage storage, GeneratorExecutionContext context)
   {
-    ContainerInitializationGenerator.Generate(storage, context);
+    if (storage.BaseToOverrides.Count == 0) return;
+    
+    var sb = new StringBuilder();
+    new GeneratedContainerInitializerModel(storage, context.Compilation.Assembly).GenerateInto(sb, 0);
+    
+    context.AddSource($"{context.Compilation.Assembly.Name}", sb.ToString());
   }
 }
