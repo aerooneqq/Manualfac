d011e6cb4a685220dd2b74e0cc1b0a428283facf
aero <aerooneQ@yandex.ru>
2023-08-02 14:04:24 +0300

    Add test sources

---
+++ test_data/sources/BeforeAfterRelationCycleTest1/source1.cs
@@ -0,0 +1,34 @@
+﻿using ManualfacAttributes;
+using System.Collections.Generic;
+
+namespace DefaultNamespace;
+
+interface IClass1
+{
+  
+}
+
+[Component, After<Class2, Class3, Class4>]
+public class Class1 : IClass1
+{
+}
+
+[Component, After<Class3, Class4>]
+public class Class2 : IClass1
+{
+}
+
+[Component, After<Class4>]
+public class Class3 : IClass1
+{
+}
+
+[Component, After<Class1>]
+public class Class4 : IClass1
+{
+}
+
+[Component, DependsOn<Internal, IEnumerable<IClass1>>()]
+public class Class5
+{
+}
---
+++ test_data/sources/BeforeAfterRelationCycleTest3/source1.cs
@@ -0,0 +1,23 @@
+﻿using ManualfacAttributes;
+using System.Collections.Generic;
+
+namespace DefaultNamespace;
+
+interface IClass1
+{
+}
+
+[Component, Before<Class2>]
+public class Class1 : IClass1
+{
+}
+
+[Component, Before<Class1>]
+public class Class2 : IClass1
+{
+}
+
+[Component, DependsOn<Internal, IEnumerable<IClass1>>()]
+public class Class5
+{
+}
