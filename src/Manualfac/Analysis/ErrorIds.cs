f6900708dd83aaee0f3cbc735d548de4ddc46d83
aero <aerooneQ@yandex.ru>
2023-08-05 15:03:47 +0300

    Separate error ids

---
+++ src/Manualfac/Analysis/ErrorIds.cs
@@ -0,0 +1,22 @@
+namespace Manualfac.Analysis;
+
+public static class ErrorIds
+{
+  public const string ErrorPrefix = "MFAC";
+  
+  private const uint DependingOnNonComponentSymbol = 1;
+  private const uint DuplicatedDependency = 2;
+  private const uint MultipleBaseComponents = 3;
+  private const uint BaseComponentIsNotClass = 4;
+  private const uint CanNotOverrideNonComponentSymbol = 5;
+
+  
+  public static string DependingOnNonComponentSymbolId { get; } = CreateErrorId(DependingOnNonComponentSymbol);
+  public static string DuplicatedDependencyId { get; } = CreateErrorId(DuplicatedDependency);
+  public static string MultipleBaseComponentsId { get; } = CreateErrorId(MultipleBaseComponents);
+  public static string BaseComponentIsNotClassId { get; } = CreateErrorId(BaseComponentIsNotClass);
+  public static string CanNotOverrideNonComponentSymbolId { get; } = CreateErrorId(CanNotOverrideNonComponentSymbol);
+
+  
+  private static string CreateErrorId(uint id) => $"{ErrorPrefix}{id.ToString().PadLeft(5, '0')}";
+}
--- src/Manualfac/Analysis/Errors.cs
+++ src/Manualfac/Analysis/Errors.cs
@@ -12,20 +12,18 @@ public static class Errors
   public static Diagnostic DependsOnNonComponentSymbol(
     AttributeSyntax node, INamedTypeSymbol componentType, INamedTypeSymbol dependantType)
   {
-    const uint ErrorId = 1;
     const string Title = "Depending on non-component symbol";
 
     var componentName = componentType.GetFullName();
     var dependantName = dependantType.GetFullName();
     var text = $"Can not reference non-component symbol (component: {componentName}, dependency: {dependantName})";
 
-    return CreateDiagnostic(ErrorId, Title, text, node);
+    return CreateDiagnostic(ErrorIds.DependingOnNonComponentSymbolId, Title, text, node);
   }
 
-  private static string CreateErrorId(uint id) => $"MFAC{id.ToString().PadLeft(5, '0')}";
 
-  private static Diagnostic CreateDiagnostic(uint errorId, string title, string text, SyntaxNode node) => 
-    Diagnostic.Create(CreateDiagnosticDescriptor(CreateErrorId(errorId), title, text), node.GetLocation());
+  private static Diagnostic CreateDiagnostic(string errorId, string title, string text, SyntaxNode node) => 
+    Diagnostic.Create(CreateDiagnosticDescriptor(errorId, title, text), node.GetLocation());
 
   private static DiagnosticDescriptor CreateDiagnosticDescriptor(string errorId, string title, string text) => 
     new(errorId, title, text, Category, DiagnosticSeverity.Error, true);
@@ -33,45 +31,38 @@ public static class Errors
   public static Diagnostic DuplicatedDependency(
     AttributeSyntax node, INamedTypeSymbol componentType, INamedTypeSymbol duplicatedDependency)
   {
-    const uint ErrorId = 2;
     const string Title = "Duplicated dependency symbol";
 
     var componentName = componentType.GetFullName();
     var duplicateName = duplicatedDependency.GetFullName();
     var text = $"Duplicated dependency (component: {componentName}, duplicated dependency: {duplicateName})";
 
-    return CreateDiagnostic(ErrorId, Title, text, node);
+    return CreateDiagnostic(ErrorIds.DuplicatedDependencyId, Title, text, node);
   }
 
   public static Diagnostic MultipleBaseComponents(AttributeSyntax node, INamedTypeSymbol componentType)
   {
-    const uint ErrorId = 3;
     const string Title = "Multiple base components";
 
     var text = $"Component {componentType.GetFullName()} declared multiple base components";
-
-    return CreateDiagnostic(ErrorId, Title, text, node);
+    return CreateDiagnostic(ErrorIds.MultipleBaseComponentsId, Title, text, node);
   }
 
   public static Diagnostic BaseComponentIsNotClass(
     AttributeSyntax node, INamedTypeSymbol componentType, INamedTypeSymbol baseType)
   {
-    const uint ErrorId = 4;
     const string Title = "Base component is not a class";
 
     var text = $"Base component {baseType.GetFullName()} of component {componentType.GetFullName()} is not a class";
-
-    return CreateDiagnostic(ErrorId, Title, text, node);
+    return CreateDiagnostic(ErrorIds.BaseComponentIsNotClassId, Title, text, node);
   }
 
   public static Diagnostic CanNotOverrideNonComponentSymbol(
     AttributeSyntax node, INamedTypeSymbol component, INamedTypeSymbol baseType)
   {
-    const uint ErrorId = 5;
     const string Title = "Can not override non-component symbol";
 
     var text = $"Component {component.GetFullName()} can not override non-component symbol {baseType.GetFullName()}";
-
-    return CreateDiagnostic(ErrorId, Title, text, node);
+    return CreateDiagnostic(ErrorIds.CanNotOverrideNonComponentSymbolId, Title, text, node);
   }
 }
